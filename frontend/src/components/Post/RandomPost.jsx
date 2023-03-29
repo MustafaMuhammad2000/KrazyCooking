948d3f653959e4e319af261481f0d0dbc2ba7894
Nicola <93399586+nicolaprimomo@users.noreply.github.com>
2023-03-28 20:23:10 -0600

    Update RandomPost.jsx

--- frontend/src/components/Post/RandomPost.jsx
+++ frontend/src/components/Post/RandomPost.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from "react-router-dom";
 const RandomPost = () => {
   const navigate = useNavigate();
   const goBack = () => {
-    navigate(-1);
+    navigate("/");
   };
   const [post, setPost] = useState([]);
   useEffect(() => {
